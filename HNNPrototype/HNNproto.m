4c203408080f286aa9fc909543912525e0c17c92
enigmatiX1983 <robertm83@gmail.com>
2016-04-08 03:35:27 -0500

    First working version for Matlab!

--- HNNPrototype/HNNproto.m
+++ HNNPrototype/HNNproto.m
@@ -2,44 +2,68 @@ function trainedNetwork = HNNProto(stimVec, respVec)
 %This function does something
 
 stimVecSize = size(stimVec)
+respVecSize = size(respVec)
 
 %%Preallocate the matrix array
 stimMatrixNormal = zeros(stimVecSize(1), stimVecSize(2));
-respMatrixNormal = zeros(stimVecSize(1), stimVecSize(2));
-hiddMatrixNormal = zeros(1, stimVecSize(2));
+respMatrixNormal = zeros(respVecSize(1), respVecSize(2));
+%hiddMatrixNormal = zeros(1, stimVecSize(2));
 
-%%Process the stimulus and response vectors
+%%Process the stimulus vector
 for n=1:stimVecSize(1)
     stimMatrixNormal(n,:) =  sigmoidNorm(stimVec(n,:));
+end
+
+%%Process the response vector
+for n=1:respVecSize(1)
     respMatrixNormal(n,:) =  sigmoidNorm(respVec(n,:));
 end
 
 %%Process the dot products to find the phase-angle difference between the complex
 %%vectors
-for m=1:stimVecSize(1)  
-    stimMatrixNormal
-    respMatrixNormal
-    for n=1:stimVecSize(2)
-        m
-        n
-        hiddMatrixNormal(1,n) = hiddMatrixNormal(1,n) + dot(ctranspose(stimMatrixNormal(m,n)'), respMatrixNormal(m,n))
-    end
-    
+%for m=1:stimVecSize(1)  
+%    stimMatrixNormal
+%    respMatrixNormal
+%    for n=1:stimVecSize(2)
+%        m
+%        n
+%        hiddMatrixNormal(1,n) = hiddMatrixNormal(1,n) + dot(ctranspose(stimMatrixNormal(m,n)'), respMatrixNormal(m,n))
+%end
+%    
+%end
+
+%create the random X matrix
+%X = exp(1i *   pi * rand(stimVecSize(2), stimVecSize(2)))
+X = zeros(stimVecSize(2), stimVecSize(2));
+
+stimMatrixNormal
+respMatrixNormal
+
+X
+
+%%Train the network
+for n=1:stimVecSize
+    stimMatrixNormal(n,:)
+    respMatrixNormal(n,:)
+    X = X + (ctranspose(stimMatrixNormal(n,:))* respMatrixNormal(n,:))
 end
 
-hiddMatrixNormal
+X
 
 testMat = [ 1 2 3 4 5 ];
-testMatNorm = sigmoidNorm(testMat)
-oneOverC = 1/sum(testMat);
-resultMatrix = zeros(1,2);
+num = sum(testMat)
 
-for m=1:5
-    resultMatrix(1,m) = dot(testMatNorm(1,m), hiddMatrixNormal(1,m)');
+R = (1/5)*sigmoidNorm(testMat)*X
+%testMatNorm = sigmoidNorm(testMat)
+%oneOverC = 1/sum(testMat);
+%resultMatrix = zeros(1,5);
+
+%for m=1:5
+%    resultMatrix(1,m) = dot(testMatNorm(1,m), hiddMatrixNormal(1,m)');
     %resultMatrix(1,m) = dot(hiddMatrixNormal(1,m), testMatNorm(1,m));
-end
+%end
 
-resultMatrix
+%resultMatrix
 
 
 
--- HNNPrototype/sigmoidNorm.m
+++ HNNPrototype/sigmoidNorm.m
@@ -2,8 +2,8 @@
 %Fun
 function [normStimVec] = sigmoidNorm(stimulus)
 
-    stdDev = std(stimulus)
-    stimMean = mean(stimulus)
+    stdDev = std(stimulus);
+    stimMean = mean(stimulus);
     
     %Get the size of the stimulus vector
     temp = size(stimulus);
@@ -15,28 +15,28 @@ function [normStimVec] = sigmoidNorm(stimulus)
     %%To catch the case 
     if stdDev == 0
         for n = 1:stimVecSize
-            nValues(n) = ( 2 * pi ) / ( 1 + exp( -( stimulus(n) - stimMean )))
+            nValues(n) = ( 2 * pi ) / ( 1 + exp( -( stimulus(n) - stimMean )));
         end
     else
         for n = 1:stimVecSize
-            nValues(n) = ( 2 * pi ) / ( 1 + exp( -( ( stimulus(n) - stimMean ) / stdDev )) )
+            nValues(n) = ( 2 * pi ) / ( 1 + exp( -( ( stimulus(n) - stimMean ) / stdDev )) );
         end        
     end
     
 
     
     %Normalize the nValues so that they fall within the unit circle
-    tempSum = sum(stimulus);
+    %tempSum = sum(stimulus);
     %stimNorm = 1 / (expected range of values)
-    stimNorm = 1 / 5;
+    %stimNorm = 1 / 5;
     
     %%Preallocat the vector
     normStimVec = zeros(1,stimVecSize);
     
     %Use Euler's formula to convert the angles into complex form
     for n = 1:stimVecSize
-        normStimVec(n) = stimNorm * stimulus(n) * ( cos(nValues(n)) + 1i * sin(nValues(n)) );
-        %normStimVec(n) = ( cos(nValues(n)) + i * sin(nValues(n)) );
+        %normStimVec(n) = stimNorm * stimulus(n) * ( cos(nValues(n)) + 1i * sin(nValues(n)) );
+        normStimVec(n) = ( cos(nValues(n)) + i * sin(nValues(n)) );
     end
     
     %normStimVecr
