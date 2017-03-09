6569577dfd7f718dec384f4fa7a5eb77a70f5d7f
Robert Martin <robertm83@gmail.com>
2017-03-08 20:27:12 -0600

    Finished HNNTest

--- HNNPrototype/HNNTest.m
+++ HNNPrototype/HNNTest.m
@@ -8,3 +8,17 @@ function [output] = HNNTest( stimFile, respFile, learningMode, epochs )
     %respVec = importdata('testdata/responselist.dat');
     
     output = HNNproto(stimVec, respVec, learningMode, epochs);
+    
+    tmpDifferenceVec = zeros(size(respVec));
+    
+    stimVecCount = size(stimVec, 1);
+    
+    %Temporary c value
+    c = size(stimVec, 2);
+    
+    %Generate actual results to compare to
+    for n=1:stimVecCount
+        tmpDifferenceVec(n) = ((1/c)*sigmoidNorm(stimVec(n,:))*output) - sigmoidNorm(respVec(n));
+    end
+    
+    tmpDifferenceVec
--- HNNPrototype/HNNproto.m
+++ HNNPrototype/HNNproto.m
@@ -1,7 +1,4 @@
 function trainedNetwork = HNNproto(stimVec, respVec, learningMode, iterations)
-%%EXAMPLE
-%
-
 %learningMode 1: Original
 %learningMode 2 : Improved
 
@@ -11,7 +8,6 @@ respVecSize = size(respVec);
 %%Preallocate the matrix array
 stimMatrixNormal = zeros(stimVecSize(1), stimVecSize(2));
 respMatrixNormal = zeros(respVecSize(1), respVecSize(2));
-%hiddMatrixNormal = zeros(1, stimVecSize(2));
 
 %%Process the stimulus vector
 for n=1:stimVecSize(1)
@@ -24,82 +20,85 @@ for n=1:respVecSize(1)
 end
     
 trainedNetwork = zeros( stimVecSize(2), respVecSize(2));
-%trainedNetwork = complex(trainedNetwork) + 1i
-%trainedNetwork = sigmoidNorm([1 2 3 4 5])'          
-
-
-%c = stimVecSize(2);
-
-for m=1:iterations
+      
+for m=1:iterations      
     %learningMode
     %1 == Original learning no Hermetian
     %2 == Improved learning no Hermetian
     %3 == Original learning Hermetian
     %4 == Improved learning Hermetian
-    
+        
     %learningMode 1; Original learning no Hermetian
     if learningMode == 1  
+          
         %Train the network
         for n=1:stimVecSize
             trainedNetwork = trainedNetwork + (ctranspose(stimMatrixNormal(n,:))* respMatrixNormal(n,:));
         end   
-    %learningMode 2; Original learning Hermetian
+    %learningMode 2; improved learning no Hermetian
     elseif learningMode == 2
+        n = 1;
+
         if m == 1
-            %Train the network to populate the correlation matrix
             trainedNetwork = trainedNetwork + (ctranspose(stimMatrixNormal(1,:))* respMatrixNormal(1,:));
+            n = 2;
         end
+
         %train the rest
-        for n=2:stimVecSize
+        while n <= stimVecSize            
             %Compute the normalization co-efficient
             c = sum(abs(stimMatrixNormal(n,:)));
             
             RPrime = (1/c)*(stimMatrixNormal(n,:) * trainedNetwork);
 
-            RDiff = respMatrixNormal(n,:) - RPrime;
-            %RDiff = dot(respMatrixNormal(n,:), RPrime)
-            
-            % X += ctranspose(S)*RDiff
+            RDiff = respMatrixNormal(n,:) - RPrime;            
+
             trainedNetwork = trainedNetwork + ((ctranspose(stimMatrixNormal(n,:))*RDiff));
+            n = n + 1; 
         end    
     %learningMode 3; Original learning Hermetian
     elseif learningMode == 3
+        n = 1;
+
         if m == 1
-            %Train the network to populate the correlation matrix
             trainedNetwork = trainedNetwork + (ctranspose(stimMatrixNormal(1,:))* respMatrixNormal(1,:));
+            n = 2;
         end
 
         %train the rest
-        for n=2:stimVecSize            
+        while n <= stimVecSize
             %Compute the normalization co-efficient
             c = sum(abs(stimMatrixNormal(n,:)));
 
             %Create Hermetian for higher order statistics
             H = (1/c)*ctranspose(stimMatrixNormal(n,:))*stimMatrixNormal(n,:);
             
-            RPrime = H * trainedNetwork;          
+            RPrime = H * trainedNetwork;
 
-            % X += ctranspose(S)*RDiff
-            trainedNetwork = trainedNetwork + ((ctranspose(stimMatrixNormal(n,:))* respMatrixNormal(n,:)) - RPrime); 
+            trainedNetwork = trainedNetwork + ((ctranspose(stimMatrixNormal(n,:))* respMatrixNormal(n,:)) - RPrime);
+            n = n + 1; 
         end
     %learningMode 4; Improved learning Hermetian
     elseif learningMode == 4
+        n = 1;
+        
         if m == 1
             trainedNetwork = trainedNetwork + (ctranspose(stimMatrixNormal(1,:))* respMatrixNormal(1,:));
+            n = 2;
         end
 
         %train the rest
-        for n=2:stimVecSize
+        while n <= stimVecSize            
             %Compute the normalization co-efficient
             c = sum(abs(stimMatrixNormal(n,:)));
 
             %Create Hermetian for higher order statistics
             H = (1/c)*ctranspose(stimMatrixNormal(n,:))*stimMatrixNormal(n,:);
             
-            RPrime = H * trainedNetwork;         
+            RPrime = H * trainedNetwork;
 
-            % X += ctranspose(S)*RDiff
             trainedNetwork = trainedNetwork + ((ctranspose(stimMatrixNormal(n,:))* respMatrixNormal(n,:)) - RPrime);
+            n = n + 1; 
         end        
         
     end
