fab15ec9bd120b29694392e3eea1b6a60b5e54aa
Robert Martin <robertm83@gmail.com>
2017-04-11 22:42:52 -0500

    Additions and Enhancements

--- HNNPrototype/HNNproto.m
+++ HNNPrototype/HNNproto.m
@@ -11,37 +11,106 @@ function trainedNetwork = HNNproto(stimVec, respVec, learningMode, epochs, symme
 
 stimVecSize = size(stimVec);
 respVecSize = size(respVec);
+reportNumber = 100;
 
-%%Preallocate the matrix array
-stimMatrixNormal = zeros(stimVecSize(1), stimVecSize(2));
-respMatrixNormal = zeros(respVecSize(1), respVecSize(2));
+%%Preallocate the matrix array for the Stimulus Vector
+%Preallocate two dimensional (one dimension x time) (sound)
+if numel(stimVecSize) == 2
+    stimMatrixNormal = zeros(stimVecSize(1), stimVecSize(2));
+%Preallocate three dimensional (two dimension x time) (black and white vision)
+elseif numel(stimVecSize) == 3
+    stimMatrixNormal = zeros(stimVecSize(1), stimVecSize(2), stimVecSize(3));
+end
+
+%%Preallocate the matrix array for the Response Vector
+%Preallocate two dimensional (one dimension x time) (sound)
+if numel(respVecSize) == 2
+    respMatrixNormal = zeros(respVecSize(1), respVecSize(2));
+%Preallocate three dimensional (two dimension x time) (black and white vision)
+elseif numel(respVecSize) == 3
+    respMatrixNormal = zeros(respVecSize(1), respVecSize(2), respVecSize(3));
+end
 
 %%Process the stimulus vector
-for n=1:stimVecSize(1)
-    if symmetryFunc == 1
-        stimMatrixNormal(n,:) =  sigmoidNorm(stimVec(n,:));
-    elseif symmetryFunc == 2
-        stimMatrixNormal(n,:) = transferFnNorm(stimVec(n,:));
+%Two dimensional case
+if numel(stimVecSize) == 2
+    for n=1:respVecSize(1)
+        if symmetryFunc == 1
+            stimMatrixNormal(n,:) =  sigmoidNorm(stimVec(n,:));
+        elseif symmetryFunc == 2
+            stimMatrixNormal(n,:) = transferFnNorm(stimVec(n,:));
+        end
+        
+        if mod(n, reportNumber) == 0
+            fprintf('Processing stimulus element %i\n', n);
+        end
+    end
+%Three dimensional case
+elseif numel(stimVecSize) == 3
+    for n=1:stimVecSize(3)
+        if symmetryFunc == 1
+            stimMatrixNormal(:,:,n) =  sigmoidNorm(stimVec(:,:,n));
+        elseif symmetryFunc == 2
+            stimMatrixNormal(:,:,n) = transferFnNorm(stimVec(:,:,n), 1);
+        end
+
+        if mod(n, reportNumber) == 0
+            fprintf('Processing stimulus element %i\n', n);
+        end
     end
 end
 
 %%Process the response vector
-for n=1:respVecSize(1)
-    if symmetryFunc == 1
-        respMatrixNormal(n,:) =  sigmoidNorm(respVec(n,:));
-    elseif symmetryFunc == 2
-        respMatrixNormal(n,:) =  transferFnNorm(respVec(n,:));
+%Two dimensional case
+if numel(respVecSize) == 2
+    for n=1:respVecSize(1)
+        if symmetryFunc == 1
+            respMatrixNormal(n,:) =  sigmoidNorm(respVec(n,:));
+        elseif symmetryFunc == 2
+            respMatrixNormal(n,:) =  transferFnNorm(respVec(n,:), 1);
+        end
+        
+        if mod(n, reportNumber) == 0
+            fprintf('Processing response element %i\n', n);
+        end
+    end
+%Three dimensional case
+elseif numel(respVecSize) == 3
+    for n=1:respVecSize(3)
+        if symmetryFunc == 1
+            respMatrixNormal(:,:,n) =  sigmoidNorm(respVec(:,:,n));
+        elseif symmetryFunc == 2
+            respMatrixNormal(:,:,n) =  transferFnNorm(respVec(:,:,n), 1);
+        end
+
+        if mod(n, reportNumber) == 0
+            fprintf('Processing response element %i\n', n);
+        end
     end
 end
-    
-trainedNetwork = zeros( stimVecSize(2), respVecSize(2));
+ 
+if numel(stimVecSize) == 2
+    trainedNetwork = zeros( stimVecSize(2), respVecSize(2));
+elseif numel(stimVecSize) == 3
+    trainedNetwork = zeros (stimVecSize(1), stimVecSize(2), respVecSize(2));
+end
+
+%temp 
+respMatrixNormal = respMatrixNormal';
               
 %learningMode 1; Original learning no Hermetian
 if learningMode == 1  
-    for m=1:epochs      
-        %Train the network
-        for n=1:stimVecSize
-            trainedNetwork = trainedNetwork + (ctranspose(stimMatrixNormal(n,:))* respMatrixNormal(n,:));
+    for m=1:epochs
+        if numel(stimVecSize) == 2
+            %Train the network
+            for n=1:stimVecSize
+                trainedNetwork = trainedNetwork + (ctranspose(stimMatrixNormal(n,:))* respMatrixNormal(n,:));
+            end
+        elseif numel(stimVecSize) == 3
+            %Train the network
+            for n=1:stimVecSize
+                trainedNetwork = trainedNetwork + (ctranspose(stimMatrixNormal(:,:,n))* respMatrixNormal(n,:));
+            end
         end
     end
 %learningMode 2; improved learning no Hermetian
--- HNNPrototype/mnistHolonet.m
+++ HNNPrototype/mnistHolonet.m
@@ -1,21 +1,28 @@
-function [ ] = mnistHolonet( learningMode, epochs, symmetryFunc )
+function [ ] = mnistHolonet( learningMode, epochs, symmetryFunc, numImagesToTrain )
 %MNISTHOLONET Summary of this function goes here
 %   Detailed explanation goes here
     mnistImages = loadMNISTImages('training_images\train-images.idx3-ubyte');
     mnistLables = loadMNISTLabels('training_labels\train-labels.idx1-ubyte');
     
-    trainedNetwork = HNNproto(mnistImages, mnistLables, learningMode, epochs, symmetryFunc);
+    %[28 28 60000]
+    stimVec = reshape(mnistImages*255, [28 28 60000]);
+    respVec = reshape(mnistLables, [1 60000]);
     
-    tmpDifferenceVec = zeros(size(mnistLables));
+    stimVec = stimVec(:,:,1:numImagesToTrain);
+    respVec = respVec(:,1:numImagesToTrain);
+
+    trainedNetwork = HNNproto(stimVec, respVec, learningMode, epochs, symmetryFunc);
     
-    stimVecCount = size(mnistImages, 1);
+    tmpDifferenceVec = zeros(size(respVec));
+    
+    stimVecCount = size(stimVec, 1);
     
     %Temporary c value
-    c = size(mnistImages, 2);
+    c = size(stimVec, 2);
     
     %Generate actual results to compare to
     for n=1:stimVecCount
-        tmpDifferenceVec(n) = ((1/c)*sigmoidNorm(mnistImages(n,:))*trainedNetwork) - sigmoidNorm(mnistLables(n));
+        tmpDifferenceVec(n) = ((1/c)*sigmoidNorm(stimVec(n,:))*trainedNetwork) - sigmoidNorm(respVec(n));
     end
      
     plotNeurons(trainedNetwork);
